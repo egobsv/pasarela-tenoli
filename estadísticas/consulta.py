88d403b58575bc1809f71f9f42271f9422f9866b
Gobierno Electrónico <31377405+egobsv@users.noreply.github.com>
2019-12-02 10:31:10 -0600

    Update consulta.py

--- estadísticas/consulta.py
+++ estadísticas/consulta.py
@@ -35,4 +35,34 @@ def llenar_plantilla() -> str:
         return request_template.format(
                 message_id_placeholder=var_message_id,
 		client_system=var_client_system,
-print("terminado") 
+		client_code=var_client_code,
+		client_server_code=var_client_server_code,
+		tiempo_inicio=var_inicio,tiempo_fin=var_fin)
+
+def post_xml_request(data: str, get_raw_stream: bool=False) -> requests.Response:
+    return requests.post(
+            "https://" + var_host, data=data.encode("utf-8"),
+            cert=(var_certificado_cliente,var_llave_cliente),verify=False,
+            headers={"Content-type": "text/xml; charset=utf-8"},
+            stream=get_raw_stream)
+
+var_peticion_soap=llenar_plantilla()
+#print("\nGenerated message ID %s for X-Road request" % (var_message_id))
+#print("Generated the following X-Road request: \n")
+#print(var_peticion_soap)
+
+response = post_xml_request( var_peticion_soap, get_raw_stream=True)
+mime_parts, raw_response = common.parse_multipart_response(response)
+if not mime_parts:
+           print("\n\n**-La peticion no pudo ser procesada, por favor revise los valores usados \n")
+           print(var_peticion_soap)
+           raise Exception("Expected a multipart response, received a plain SOAP response")
+
+fecha=time.strftime("%Y-%m-%d", time.gmtime())
+
+json_payload = common.get_json_payload_as_string(mime_parts[1])
+var_respuesta=var_respuesta+fecha+".json"
+archivo = open(var_respuesta,"w+") 
+archivo.write(json_payload);
+archivo.close();
+print("Se guardo la respuesta en "+var_respuesta) 
