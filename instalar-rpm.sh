f3674d8dfd92b5abd5e3efbae6176d766527fdf6
Gobierno Electrónico <31377405+egobsv@users.noreply.github.com>
2019-12-03 14:41:27 -0300

    Update instalar-rpm.sh

--- instalar-rpm.sh
+++ instalar-rpm.sh
@@ -7,14 +7,14 @@ echo "LC_ALL=en_US.UTF-8">> /etc/environment;
 mkdir /opt/tenoli;
 mkdir /etc/xroad;
 chown -R xroad:xroad /etc/xroad;
-cd /opt/tenoli/;
-cp /vagrant/debs6.22/rpms/* /opt/tenoli/;
 yum install -y yum-utils;
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm;
 yum update -y;
 yum install -y java-1.8.0-openjdk.x86_64;
 yum install -y crudini rlwrap nginx policycoreutils-python net-tools expect postgresql-server postgresql-contrib;
 
+#carpeta con paquetes RPM
+cd /opt/tenoli/;
 yum --disablerepo=* localinstall -y *.rpm;
 
 #Agregar usuario administrador
