aa505dd063daf8e2c61e7647a9738d7833de097b
Gobierno Electrónico <31377405+egobsv@users.noreply.github.com>
2019-12-16 19:06:59 -0600

    Update instalar-rpm.sh

--- instalar-rpm.sh
+++ instalar-rpm.sh
@@ -36,3 +36,9 @@ net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
 ~# sysctl -p
+
+#Deshabilitar Sitio de Nginx
+~# nano /etc/nginx/nginx.conf
+--Comentar Linea 40
+# listen        [::}:80 default_server;
+~# systemctl restart nginx
